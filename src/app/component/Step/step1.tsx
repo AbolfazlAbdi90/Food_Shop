df20440904dcee6a4fb3e05d7de010c3bfe0e101
Abolfazl Abdi <abdiabolfazl@gmail.com>
2025-04-14 08:49:15 -0700

    we create the step1

--- src/app/component/Step/step1.tsx
+++ src/app/component/Step/step1.tsx
@@ -1,7 +1,7 @@
 // Step1.tsx
 export default function StepOne() {
   return (
-    <div>
+     <div>
       <div className="w-[240px] h-[292px] relative top-[0px] left-[40px] bg-[#98A8B8] rounded-2xl"></div>
       <h1 className="relative top-[63px] text-[27px] left-[60]">All your favorites</h1>
       <p className="text-[#646982] relative top-[92px] left-[9]">Get all your loved foods in one once place,<br/>
